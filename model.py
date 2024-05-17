b4cceed2efc66f80471004fb58baaed463af0fec
jrc15 <53946681+jrc15@users.noreply.github.com>
2024-05-17 15:46:38 +0100

    Update model.py
    Removed old code - mostly print commands

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 # model.py
 # J. Cook
 # Email: "jrc15@aber.ac.uk"
-# This is the model part of the QT Application - balsam - extract & map
+# This is the model part of the QT Application - SATT
 
 from shapely.geometry import LineString, Point
 import exiftool as exiftool # need exiftool software in same directory as code (exiftool.exe) # ensure pyexiftool in installed via pip
@@ -27,19 +27,14 @@ class Model:
         self.fileContent = ""
 
 
-    def extractBalsam( self, dirPath, flwDir, outPath, results):
-        print("In MODEL: Unpacking Results")
         # unpack results
         Lh, Ls, Lv, Uh, Us, Uv, Fshp, Fx, Fy, Os, Bbox = results
-        print('In Model: ', Lh, Ls, Lv, Uh, Us, Uv, Fshp, Fx, Fy, Os, Bbox)
         
         ###### Automatic Threshold Extraction of Balsam Objects fom Oblique Images ######
 
         # create annotation file name
         vectorNames = dirPath.split("/")[-1]
         annotation_csv = outPath + '/' + vectorNames + '_annotations.csv'
-        print(annotation_csv)
-        print(outPath + '/' + vectorNames)
 
         # get list of images from directory
         imgList = glob.glob(dirPath + '\*.JPG')
@@ -57,7 +52,6 @@ class Model:
         # loop through each image,
         # pull out flowers and annotate
         for i, img in enumerate(imgList):
-            #print("Processing: ",i, " of ", len(imgList), end=" \r")
 
             # emit signal
             progress = int(100*(i+1)/len(imgList))
@@ -163,8 +157,6 @@ class Model:
             writer = csv.writer(f)
             writer.writerows(_annotations)
 
-        print('Automatic Threshold Extraction Completed.')
-
         # if check box for 'Only CNN Training' is ticked end script
         if self.checkCNNBox.isChecked():
             sys.exit()
@@ -212,9 +204,6 @@ class Model:
                 flower_files.append(n)
                 count.append(x)
 
-        #print('flower files = ', len(flower_files))
-        #print('filenames = ', len(filenames))
-
         flower_count = count
 
         # get list of filesnames containing flowers
@@ -222,9 +211,6 @@ class Model:
 
         # get mean balsam object heights
         for i, name in enumerate(filenames):
-
-            #self.metricsBar.setValue(i+1)
-            #print("Processing: ",i, " of ", len(filenames), end=" \r")
 
             new_df = df[df[columns[0]]==name]
             img_heights = []
@@ -281,8 +267,6 @@ class Model:
             seg_1_heights.append(seg_1_mean)
             seg_5_heights.append(seg_5_mean)
 
-        print('Generated Height Metrics for Flowers Completed.')
-
         ###### calculate distance of flowers in image from image capture point ######
 
         # get list of files
@@ -297,8 +281,6 @@ class Model:
             focal = et.get_tag_batch('FocalLength',images)
             image_height = et.get_tag_batch('ImageHeight',images)
             fov = et.get_tag_batch('FOV', images)
-
-        #print(len(images), len(flower_count), len(seg_1_heights), len(seg_5_heights))
         
         adjusted_gimbal = []
         adjusted_flight = []
@@ -354,8 +336,6 @@ class Model:
              'flower_count': flower_count
             })
 
-        #print(df.head())
-
         ### calculate distance for each image ###
 
         remove_ind = []
@@ -368,14 +348,12 @@ class Model:
 
         # open file in geopandas
         gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.long, df.lat), crs="EPSG:4326")
-        #print(gdf.head())
 
         # calculate object distance from coordinates in given orientation
         focal_length = focal[0]
         real_object_height = Os                               ####### Add as button/parameter in function/GUI #######
         sensor_height = 8.8
 
-        #print('focal length = ', focal[0])
         for n in range(0,len(focal)):
 
             # image and object heights
@@ -465,8 +443,6 @@ class Model:
             capLine = LineString([Point(longitude[n], lat[n]), Point(centreLon, centreLat)])
             capLines.append(capLine)
 
-            #print("\r" + str(n+1) + " of " + str(len(focal)) + "\r", end="")
-
         # add new column to df
         df['distances'] = distances
 
@@ -482,8 +458,6 @@ class Model:
             new_gdf.to_file(outPath + '/' + vectorNames + '_FOV_Lines.geojson', driver='GeoJSON')
             
         except Exception as e: print(e)
-            
-        #print(outPath + '/' + vectorNames + '_FOV_Lines.geojson')
 
         # and points:
         point_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(centre_lons, centre_lats), crs = gdf.crs)
@@ -500,15 +474,12 @@ class Model:
         capture_dist = gpd.GeoDataFrame(df, geometry=capLines, crs = gdf.crs)
         capture_dist = capture_dist.to_crs("EPSG:27700")
         capture_dist.to_file(outPath + '/' + vectorNames + '_Capture_Lines.geojson', driver='GeoJSON')
-
-        print('Completed Vector Generation')
         
         
     def processAdvance( self, filename, results):
     
         # unpack results
         Lh, Ls, Lv, Uh, Us, Uv, Fshp, Fx, Fy, Os, Bbox = results
-        print('In Model: ', Lh, Ls, Lv, Uh, Us, Uv, Fshp, Fx, Fy)
         
         ###### Automatic Threshold Extraction of Balsam Objects fom Oblique Images ######
         #outImg = flwDir + img[len(dirPath):-4] + '_flowers.png'  might need to add a tmp folder to input folder and save localy to there.
